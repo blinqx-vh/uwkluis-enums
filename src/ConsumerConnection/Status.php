851a36020c4ece8906f53dc71aa478660b8017b1
Erwin Deckers <e.deckers@hypotheekbond.nl>
2018-09-05 11:30:49 +0200

    Adds a ConsumerConnection\Status enum

---
+++ src/ConsumerConnection/Status.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace UwKluis\Enums\ConsumerConnection;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class Status
+ */
+final class Status extends Enum
+{
+    /** @var string  */
+    const NEW = 'new';
+    /** @var string  */
+    const INVITE_SENT = 'invite_sent';
+    /** @var string  */
+    const PENDING = 'pending';
+    /** @var string  */
+    const ACTIVE = 'active';
+    /** @var string  */
+    const DECLINED = 'declined';
+    /** @var string  */
+    const REVOKED = 'revoked';
+
+    /**
+     * @var array
+     */
+    public static $descriptions = [
+        'en_GB' => [
+            self::NEW         => 'A connection has been requested',
+            self::INVITE_SENT => 'An invite has been sent to the consumer',
+            self::PENDING     => 'The consumer has opened the invite, but has not accepted yet',
+            self::ACTIVE      => 'The connection is active',
+            self::DECLINED    => 'The consumer has opened the invite, but declined it',
+            self::REVOKED     => 'The consumer revoked the connection',
+        ],
+        'nl_NL' => [
+            self::NEW         => 'Er is een verbinding aangevraagd',
+            self::INVITE_SENT => 'Er is een uitnodiging verstuurd naar de consument',
+            self::PENDING     => 'De consument heeft de uitnodiging geopend maar nog niet de rechten geaccepteerd',
+            self::ACTIVE      => 'De verbinding is actief',
+            self::DECLINED    => 'De consument heeft de uitnodiging geopend maar de rechten geweigerd',
+            self::REVOKED     => 'De consument heeft de verbinding verbroken',
+        ],
+    ];
+
+    /**
+     * @return string
+     */
+    public function getDescription(string $lang): string
+    {
+        return self::$descriptions[$lang][$this->getValue()];
+    }
+}
